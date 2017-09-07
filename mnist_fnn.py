f6e93869564b57765eeda33ce8c8a76443145b9b
tarun <tarunsuper@gmail.com>
2017-09-07 19:15:02 -0400

    can finally read tfrecord!!

--- mnist_fnn.py
+++ mnist_fnn.py
@@ -6,6 +6,7 @@ import argparse
 import os.path
 import sys
 import time
+import skimage.io as io
 
 import tensorflow as tf
 
@@ -13,6 +14,49 @@ import numpy as np
 #from tensorflow.examples.tutorials.mnist import input_data as mnist_data
 
 #mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+
+
+def read_and_decode(queue):
+	reader = tf.TFRecordReader()
+	_, serialized_example = reader.read(filename_queue)
+	features = tf.parse_single_example(
+      serialized_example,
+      # Defaults are not specified since both keys are required.
+      features={
+          'image_raw': tf.FixedLenFeature([], tf.string),
+          'label': tf.FixedLenFeature([], tf.string),
+          'height': tf.FixedLenFeature([], tf.int64),
+          'width': tf.FixedLenFeature([], tf.int64),
+          'depth': tf.FixedLenFeature([], tf.int64)
+      })
+	#image = np.fromstring(features['image_raw'], dtype=np.uint8)
+	image = tf.decode_raw(features['image_raw'],tf.float32)
+	label = tf.decode_raw(features['label'], tf.float32)
+
+	height = tf.cast(features['height'], tf.int32)
+	width = tf.cast(features['width'], tf.int32)
+	depth = tf.cast(features['depth'], tf.int32)
+
+	#image_shape = tf.pack([height, width, depth])
+	image2 = tf.reshape(image, [height, width, depth])
+	return image2,label
+
+
+
+
+
+TRAIN_FILE = '/home/tarun/mine/tensorflow_examples/tensorflow-utils/train.tfrecords'
+
+
+num_epochs = 1
+filename_queue = tf.train.string_input_producer(
+        [TRAIN_FILE])
+
+    # Even when reading in multiple threads, share the filename
+    # queue.
+
+img,label = read_and_decode(filename_queue)
 
 
 b = tf.Variable(tf.zeros([10]))
@@ -26,13 +70,43 @@ output = tf.nn.softmax(tf.matmul(tf.reshape(x,[-1,784]),W)+b)
 
 loss = -tf.reduce_sum(y*tf.log(output)) 
 
-step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+#step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-TRAIN_FILE = 'train.tfrecords'
+
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord,sess=sess)
+
+img,anno = sess.run([img,label])
+
+
+print (img.shape)
+
+#print(img[0, :, :, :].shape)
+#print (sess.run(label))
+
+# Wait for threads to finish.
+#coord.join(threads)
+#sess.close()
+
+
+sys.exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
 
 for i in range(1000):
 	#batchx, batchy = mnist.train.next_batch(100)
